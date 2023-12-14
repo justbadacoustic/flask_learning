6ed476c9d576f62c5bc35cf003cfc299b79d0d2c
Antony Mead <antony.mead@live.com>
2023-12-14 10:16:51 +0000

    Following the course - fleshing out

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
 from markupsafe import escape
 import datetime
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
-@app.route('/')
-@app.route('/index/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task.'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', utc_dt=datetime.datetime.utcnow(), tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try: 
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        'There was a problem deleting that task'
 
 @app.route('/about/')
 def about():
@@ -40,3 +76,8 @@ def greet_user(user_id):
         return '<h2>Hi {}</h2>'.format(users[user_id])
     except:
         abort(404)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
